e58e5b8e21c358500d36118d4761a82e6438372e
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2020-05-14 22:04:03 +0300

    Добавил попытку добавления заявки

--- needle_bot.py
+++ needle_bot.py
@@ -8,7 +8,7 @@ token = f"{txt}"
 print(datetime.now())
 client = openapi.api_client(token)
 
-t_from = datetime(2020, 3, 9, 15, 0, 0,tzinfo=timezone('Europe/Moscow'))
+t_from = datetime(2020, 3, 10, 14, 59, 0,tzinfo=timezone('Europe/Moscow'))
 t_to = datetime(2020, 3, 10, 15, 0, 0,tzinfo=timezone('Europe/Moscow'))
 
 
@@ -16,8 +16,12 @@ t_to = datetime(2020, 3, 10, 15, 0, 0,tzinfo=timezone('Europe/Moscow'))
 #                                     broker_account_id=2010122667)
 
 x = client.market.market_candles_get(figi='BBG004S686W0', _from=t_from.isoformat(),
-                                     to=t_to.isoformat(), interval="DAY")
+                                     to=t_to.isoformat(), interval="1min")
 y = client.market.market_search_by_figi_get(figi='BBG004S686W0')
+
+
+client.orders.orders_limit_order_post(broker_account_id=2010122667, figi='BBG004S686W0',
+                                          operation="buy", lots=1, price=2.4, limit_order_request=True)
 
 print(x)
 print(y)
